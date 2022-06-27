cfa6c1b974ea5b276b7a167e5cfc6ed66090cf88
Abdoul Aziz BANDE <bandeabdelaziz3@gmail.com>
2022-06-27 23:06:27 +0000

    alx-higher_level_programming

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print(f"{88+10} Battery street")
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(str[:9],str.replace("Holberton School", "SchoolHolberton"),str.replace("Holberton School", "SchoolHolberton"),str[9:16])
+print(str[:9])
